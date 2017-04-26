756dbadf61d52d09d41c19f3a082b7a3de32bd8f
Manuel-Rodrigo Cabello Malagon <Manuel-Rodrigo Cabello Malagon>
2017-04-26 08:16:14 +0200

    Initial commit

---
+++ AzureRelayCore/Domain/Tweets/Entities/TweetInfo.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace AzureRelayCore.Domain.Tweets.Entities
+{
+    public class TweetInfo
+    {
+        public string UrlUserImage { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedByName { get; set; }
+        public string CreatedByScreenName { get; set; }
+        public int FavoriteCount { get; set; }
+        public int RetweetedCount { get; set; }
+    }
+}
---
+++ AzureRelayCore/Infrastructure/Services/TwitterStream/Extensions/TweetinviExtension.cs
@@ -0,0 +1,22 @@
+﻿using AzureRelayCore.Domain.Tweets.Entities;
+using Tweetinvi.Models;
+
+namespace AzureRelayCore.Infrastructure.Services.TwitterStream.Extensions
+{
+    public static class TweetinviExtension
+    {
+        public static TweetInfo ToTweetInfo(this ITweet tweetinviModel)
+        {
+            return new TweetInfo()
+            {
+                UrlUserImage = tweetinviModel.CreatedBy.ProfileImageUrl400x400,
+                CreatedByName = tweetinviModel.CreatedBy.Name,
+                CreatedByScreenName = tweetinviModel.CreatedBy.ScreenName,
+                CreatedAt = tweetinviModel.CreatedAt,
+                Text = tweetinviModel.Text,
+                RetweetedCount = tweetinviModel?.RetweetedTweet?.RetweetCount ?? 0,
+                FavoriteCount = tweetinviModel?.RetweetedTweet?.FavoriteCount ?? 0
+            };
+        }
+    }
+}
